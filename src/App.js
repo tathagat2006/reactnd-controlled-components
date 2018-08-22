c32b0a0cf2fdeb4e1676a7009781657ecbef10a1
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-08-22 16:40:53 +0000

    controlled components based solution proposed
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  state= {
+  	query:''
+  }
+
+updateQuery = (query) => {
+ 	this.setState(() => ({
+    	query:query
+    }))
+}
+
   render() {
+    const { query } = this.state
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
+          <h1 className="App-title">React-Controlled Components</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input type="text" placeholder="Say Something" value = {query} onChange = {(event) => {this.updateQuery(event.target.value)}}/>
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{this.state.query}</p>
         </div>
       </div>
     );
